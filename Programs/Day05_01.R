fc2e4cbb6a39f1ce84db2c83c69e9e8bec54938f
szimmer <stephaniezimmer@gmail.com>
2019-12-07 15:59:02 -0500

    Day 5 working with test cases

--- Programs/Day05_01.R
+++ Programs/Day05_01.R
@@ -4,74 +4,92 @@ input <- read_csv("Data/Day05_input.txt", col_names = FALSE)
 
 ProgramOutput <- Day5Input <- as.vector(t(input))
 
-input <- read_csv("Data/Day02_input.txt", col_names = FALSE)
+# input <- read_csv("Data/Day02_input.txt", col_names = FALSE)
+# 
+# ProgramOutput <- Day2Input <- as.vector(t(input))
+# ProgramOutput[2] <- 12
+# ProgramOutput[3] <- 2
 
-ProgramOutput <- Day2Input <- as.vector(t(input))
-ProgramOutput[2] <- 12
-ProgramOutput[3] <- 2
 
- 
-Intcode <- function(ProgramOutput){
+Intcode <- function(ProgramOutput, InputVal){
   Steps <- ProgramOutput
   curidx <- 1
   Stop <- FALSE
   
   while (curidx < length(ProgramOutput) & !Stop){
     inst <- str_pad(ProgramOutput[curidx], 5, "left", "0")
-    opcode <- str_sub(inst, 4, 5) 
-    modes <- str_sub(inst, 1, 3) %>% str_split(pattern="", simplify=TRUE) %>% 
-      as.vector() %>%
-      as.numeric()
     
+    opcode <- str_sub(inst, 4, 5) 
+    modes <- str_sub(inst, 1, 3) %>% 
+      str_split(pattern="", simplify=TRUE) %>% 
+      as.vector() %>%
+      as.numeric() %>% 
+      rev() #reverse because it is read right to left
+     
     if (opcode=="99"){
       Stop <- TRUE
       break
     } else if (opcode=="01"){
       #Do summation
-      ImmVals <- (ProgramOutput[(curidx+1):(curidx+3)] ) 
-      ImmVals[3] <- ImmVals[3] + 1.0
-      PosVals <- ProgramOutput[ImmVals] %>% as.double()
-      UseVals <- if_else(modes==0, PosVals, ImmVals)
-      print(ImmVals)
-      print(PosVals)
-      print(UseVals)
-      ProgramOutput[UseVals[3]] <- sum(UseVals[1:2])
+      ImmVals <- (ProgramOutput[(curidx+1):(curidx+2)] )  %>% as.double()
+      PosVals <- ProgramOutput[ImmVals+1] %>% as.double()
+      UseVals <- if_else(modes[1:2]==0, PosVals, ImmVals)
+      if (modes[3]==0){
+        ProgramOutput[ProgramOutput[curidx+3]+1] <- sum(UseVals)
+      } else {
+        stop("Parameters that an instruction writes to will never be in immediate mode.")
+      }
       curidx <- curidx + 4
     } else if (opcode=="02"){
       #Do multiply
-      ImmVals <- (ProgramOutput[(curidx+1):(curidx+3)] ) 
-      ImmVals[3] <- ImmVals[3] + 1.0
-      PosVals <- ProgramOutput[ImmVals] %>% as.double()
-      UseVals <- if_else(modes==0, PosVals, ImmVals)
-      print(ImmVals)
-      print(PosVals)
-      print(UseVals)
-      
-      ProgramOutput[UseVals[3]] <- prod(UseVals[1:2])
+      ImmVals <- (ProgramOutput[(curidx+1):(curidx+2)] )  %>% as.double()
+      PosVals <- ProgramOutput[ImmVals+1] %>% as.double()
+      UseVals <- if_else(modes[1:2]==0, PosVals, ImmVals)
+      if (modes[3]==0){
+        ProgramOutput[ProgramOutput[curidx+3]+1] <- prod(UseVals)
+      } else {
+        stop("Parameters that an instruction writes to will never be in immediate mode.")
+      }
       curidx <- curidx + 4
     } else if (opcode=="03"){
       # Opcode 3 takes a single integer as input and saves it to the position 
       # given by its only parameter. 
       # For example, the instruction 3,50 would take an input value 
       # and store it at address 50.
+      Par <- ProgramOutput[curidx+1]   %>% as.double()
+      # mode should always be 0 - check for this but that is expected
+      if (modes[1]==0){
+        ProgramOutput[Par+1] <- 1 # Input is 1
+      } else{
+        stop("Parameters that an instruction writes to will never be in immediate mode.")
+      }
+      curidx <- curidx + 2
     } else if (opcode=="04"){
       # Opcode 4 outputs the value of its only parameter. 
       # For example, the instruction 4,50 would output the value at address 50.
-      
+      # can use mode, would output value of 50 if mode is 1
+      ImmVals <- (ProgramOutput[(curidx+1)] )  %>% as.double()
+      PosVals <- ProgramOutput[ImmVals+1] %>% as.double()
+      # mode should always be 0 - check for this but that is expected
+      if (modes[1]==0){
+        print(str_c("Index: ", curidx, ", Output: ", PosVals))
+      } else if (modes[1]==1){
+        print(str_c("Index: ", curidx, ", Output: ", PosVals))
+      }
+      curidx <- curidx + 2
     }
     
-    Steps <- cbind(Steps, ProgramOutput)
-
+  
     
   }
   
   
-  Steps_df <- Steps %>% as_tibble(.name_repair = "unique") %>%
-     set_names(str_c("X", 1:ncol(Steps)))
-
-  return(list(ProgramOutput=ProgramOutput, Steps=Steps))
-  # return(ProgramOutput)
+  return(ProgramOutput)
 }
 
-Day2Try <- Intcode(c(1,1,1,4,99,5,6,0,99))
-Day2Try[[1]]
+# TestCases
+Intcode(c(3, 0, 4, 0, 99))
+Intcode(c(3, 0, 4, 0, 104, 0 , 99))
+Intcode(c(3, 0, 4, 0, 104, 0, 99))
+Intcode(c(4, 0, 1101,100,-1,4,0))
+

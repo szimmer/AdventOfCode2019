579bfe8360a3baf66c82211d807f41f57b6d2936
szimmer <stephaniezimmer@gmail.com>
2019-12-07 21:23:59 -0500

    Day 5, part 1 solution, finally!

--- Programs/Day05_01.R
+++ Programs/Day05_01.R
@@ -11,14 +11,19 @@ ProgramOutput <- Day5Input <- as.vector(t(input))
 # ProgramOutput[3] <- 2
 
 
-Intcode <- function(ProgramOutput, InputVal){
+Intcode <- function(ProgramOutput, tmpmax=NULL){
   Steps <- ProgramOutput
   curidx <- 1
   Stop <- FALSE
-  
-  while (curidx < length(ProgramOutput) & !Stop){
-    inst <- str_pad(ProgramOutput[curidx], 5, "left", "0")
+  if (is.null(tmpmax)){
+    tmpmax <- length(ProgramOutput)
+  }
+  Counter <- 0
+  while (curidx < tmpmax ){
+    Counter <- Counter + 1
     
+    # Parse the instruction into opcode and modes
+    inst <- str_pad(ProgramOutput[curidx], 5, "left", "0")
     opcode <- str_sub(inst, 4, 5) 
     modes <- str_sub(inst, 1, 3) %>% 
       str_split(pattern="", simplify=TRUE) %>% 
@@ -27,26 +32,37 @@ Intcode <- function(ProgramOutput, InputVal){
       rev() #reverse because it is read right to left
      
     if (opcode=="99"){
-      Stop <- TRUE
       break
     } else if (opcode=="01"){
       #Do summation
-      ImmVals <- (ProgramOutput[(curidx+1):(curidx+2)] )  %>% as.double()
-      PosVals <- ProgramOutput[ImmVals+1] %>% as.double()
-      UseVals <- if_else(modes[1:2]==0, PosVals, ImmVals)
+      Res <- 0
+      for (i in 1:2){
+        if (modes[i]==0){ # positional mode
+          Res <- Res + ProgramOutput[ProgramOutput[curidx+i]+1]
+        } else{ # immediate mode
+          Res <- Res + ProgramOutput[curidx+i]
+        }
+      }
+
       if (modes[3]==0){
-        ProgramOutput[ProgramOutput[curidx+3]+1] <- sum(UseVals)
+        ProgramOutput[ProgramOutput[curidx+3]+1] <- Res
       } else {
         stop("Parameters that an instruction writes to will never be in immediate mode.")
       }
       curidx <- curidx + 4
     } else if (opcode=="02"){
       #Do multiply
-      ImmVals <- (ProgramOutput[(curidx+1):(curidx+2)] )  %>% as.double()
-      PosVals <- ProgramOutput[ImmVals+1] %>% as.double()
-      UseVals <- if_else(modes[1:2]==0, PosVals, ImmVals)
+      Res <- 1
+      for (i in 1:2){
+        if (modes[i]==0){ # positional mode
+          Res <- Res * ProgramOutput[ProgramOutput[curidx+i]+1]
+        } else{ # immediate mode
+          Res <- Res * ( ProgramOutput[curidx+i])
+        }
+      }
+      
       if (modes[3]==0){
-        ProgramOutput[ProgramOutput[curidx+3]+1] <- prod(UseVals)
+        ProgramOutput[ProgramOutput[curidx+3]+1] <- Res
       } else {
         stop("Parameters that an instruction writes to will never be in immediate mode.")
       }
@@ -72,19 +88,20 @@ Intcode <- function(ProgramOutput, InputVal){
       PosVals <- ProgramOutput[ImmVals+1] %>% as.double()
       # mode should always be 0 - check for this but that is expected
       if (modes[1]==0){
-        print(str_c("Index: ", curidx, ", Output: ", PosVals))
+        print(str_c("Step: ", Counter, ", Output: ", PosVals))
       } else if (modes[1]==1){
-        print(str_c("Index: ", curidx, ", Output: ", PosVals))
+        print(str_c("Step: ", Counter, ", Output: ", ImmVals))
       }
       curidx <- curidx + 2
     }
     
-  
+  Steps <- cbind(Steps, ProgramOutput)
     
   }
   
-  
-  return(ProgramOutput)
+  Steps <- t(Steps) 
+  row.names(Steps) <- NULL
+  return(list(ProgramOutput=ProgramOutput, Steps=Steps))
 }
 
 # TestCases
@@ -93,3 +110,4 @@ Intcode(c(3, 0, 4, 0, 104, 0 , 99))
 Intcode(c(3, 0, 4, 0, 104, 0, 99))
 Intcode(c(4, 0, 1101,100,-1,4,0))
 
+j <- Intcode(Day5Input)

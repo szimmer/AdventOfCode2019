d6a24e669258535d7aeda3b69e58aff57f22b882
szimmer <stephaniezimmer@gmail.com>
2019-12-07 22:50:54 -0500

    Running some test cases and still de-bugging

--- Programs/Day05_02.R
+++ Programs/Day05_02.R
@@ -13,13 +13,22 @@ ProgramOutput <- Day5Input <- as.vector(t(input))
 
 Intcode <- function(ProgramOutput, InputVal=1, tmpmax=NULL){
   Steps <- ProgramOutput
+  prevcuridx <- 0
   curidx <- 1
-  Stop <- FALSE
+  
   if (is.null(tmpmax)){
     tmpmax <- length(ProgramOutput)
   }
   Counter <- 0
+  HistIDX <- NULL
   while (curidx < tmpmax ){
+    HistIDX <- c(HistIDX, curidx)
+    if (curidx != prevcuridx){
+      prevcuridx <- curidx
+    } else{
+      break # this means the index didn't change at all and is stuck
+    }
+    
     Counter <- Counter + 1
     
     # Parse the instruction into opcode and modes
@@ -134,7 +143,7 @@ Intcode <- function(ProgramOutput, InputVal=1, tmpmax=NULL){
       ValV <- rep(NA, 2)
       for (i in 1:2){
         if (modes[i]==0){ # positional mode
-          ValV[i] ProgramOutput[ProgramOutput[curidx+i]+1]
+          ValV[i] <- ProgramOutput[ProgramOutput[curidx+i]+1]
         } else{ # immediate mode
           ValV[i] <- ProgramOutput[curidx+i]
         }
@@ -145,6 +154,7 @@ Intcode <- function(ProgramOutput, InputVal=1, tmpmax=NULL){
       } else{
         ProgramOutput[ProgramOutput[curidx+3]+1] <- 0
       }
+      curidx <- curidx + 4
 
     } else if (opcode=="08"){
       # Opcode 8 is equals: if the first parameter is equal to the second 
@@ -153,7 +163,7 @@ Intcode <- function(ProgramOutput, InputVal=1, tmpmax=NULL){
       ValV <- rep(NA, 2)
       for (i in 1:2){
         if (modes[i]==0){ # positional mode
-          ValV[i] ProgramOutput[ProgramOutput[curidx+i]+1]
+          ValV[i] <- ProgramOutput[ProgramOutput[curidx+i]+1]
         } else{ # immediate mode
           ValV[i] <- ProgramOutput[curidx+i]
         }
@@ -164,21 +174,69 @@ Intcode <- function(ProgramOutput, InputVal=1, tmpmax=NULL){
       } else{
         ProgramOutput[ProgramOutput[curidx+3]+1] <- 0
       }
+      curidx <- curidx + 4
+    } else {
+      stop("Opcode isn't valid")
     }
     
-  Steps <- cbind(Steps, ProgramOutput)
+    
+    Steps <- cbind(Steps, ProgramOutput)
     
   }
   
   Steps <- t(Steps) 
   row.names(Steps) <- NULL
-  return(list(ProgramOutput=ProgramOutput, Steps=Steps))
+  return(list(ProgramOutput=ProgramOutput, Steps=Steps, HistIDX=HistIDX))
 }
 
-# TestCases
-Intcode(c(3, 0, 4, 0, 99))
-Intcode(c(3, 0, 4, 0, 104, 0 , 99))
-Intcode(c(3, 0, 4, 0, 104, 0, 99))
-Intcode(c(4, 0, 1101,100,-1,4,0))
 
-j <- Intcode(Day5Input)
+# #Here's a larger example:
+# # The program will then output 999 if the input value is below 8, output 1000 
+# # if the input value is equal to 8, or output 1001 if the input value is greater 
+# # than 8.
+longp <- c(3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+           1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+           999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99)
+j7 <- Intcode(longp, InputVal = 1)
+j7 <- Intcode(longp, InputVal = 8)
+j7 <- Intcode(longp, InputVal = 9)
+j7[[3]]
+Steps <- j7[[2]]
+Steps[1, 1:2] # Save 9 to positon 22
+Steps[1:2, 22]
+Steps[2, 3:6] #Check if value at 22 == 8
+Steps[2, 22] #doesn't equal so save a 0 at 21
+Steps[1:3, 21]
+Steps[3, 7:10] # ....
+
+# j <- Intcode(Day5Input, InputVal = 5)
+# Steps <- j[[2]]
+# #Checking steps
+# Steps[1, 1:2] # Should take input and save to position 226
+# Steps[1:2, 226]
+# Steps[2, 3:6] # Should take value at 226 and value at 7 and save to position 7
+# Steps[2, 226]+Steps[2,7]
+# Steps[1:3, 7]
+# Steps[3, 7:9] # since 1!=0, jump to 239 for inxtructions (immediate evaluation)
+# Steps[4, 239:241] #since 0==0, go to 242
+# Steps[5, 242:244] #since 227!=0, jump to position at 248
+# Steps[6, 248:250] # find value at 228 - check if it is 0
+# Steps[6,228] # It is 0 so just go to next step
+# Steps[7, 251:253] # find value at 1 and check if it is 0
+# Steps[7, 1] # It is not 0 so jump to 257
+# Steps[8, 257:259] #227!=0 so go to next instruction
+# Steps[9, 260:262] # It is 0 so jump, pointer goes to 266
+# Steps[10, 266:268] # Check whether value at positon 0[1 in R]==0
+# Steps[10, 1] # This is not 0 so do not jump and just go to 269
+# Steps[11, 269:271] #Check whether value at position 227[228 in R]==0
+# Steps[11, 228] # this is 0 so jump to 275
+# Steps[12, 275:277] #1==1 so jump to 281
+# Steps[13, 281:284] # Add value at postion 226 to value at 226 and put it in 226
+# Steps[13:14, 226]
+# Steps[14, 285:288] # Add 294 and 0 and put it in first position
+# Steps[1:15, 1] 
+# Steps[15, 289:291] # jump if true, 1=1... this jumps back to 1
+# Steps[16, 1:4]
+# 
+# j[[3]]
+# #Expect curidx to be c(1,3,7,239,242,248,251,257, 260, 266, 269, 275, 281, 285, 289)
